91798d5b45d01bc26281ecfb6e5ef01524aa7d78
aleksandarkoprivica <aleksandar.kop@gmail.com>
2020-05-01 00:09:50 +0200

    Change text

--- koprivica/src/App.js
+++ koprivica/src/App.js
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a href="https://www.linkedin.com/in/aleksandarkoprivica1/">Aleksandar Koprivica</a> is working on that.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
