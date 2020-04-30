8e6f20e15df90114bcc97e25d84e16d63da705fe
aleksandarkoprivica <aleksandar.kop@gmail.com>
2020-04-30 22:27:37 +0200

    Updates

---
+++ precache-manifest.efba9631d2234094e4553e3c6e6cb676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a2faf58cb97261ad628c071d3cb5d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c641a87a2c77276f0de",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fae168073d9e52efdd95",
+    "url": "/static/js/2.b18cbed1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b18cbed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c641a87a2c77276f0de",
+    "url": "/static/js/main.3c3cb757.chunk.js"
+  },
+  {
+    "revision": "a5c3d77ca4f8906de5b3",
+    "url": "/static/js/runtime-main.1c057bca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/koprivica-website/precache-manifest.536df336a0246be630d7a03af3857849.js"
+  "/precache-manifest.efba9631d2234094e4553e3c6e6cb676.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/koprivica-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
