cb7aad4664ef254e9833aaf5e53cef59271f7890
aleksandarkoprivica <aleksandar.kop@gmail.com>
2020-04-30 21:38:58 +0200

    Updates

---
+++ precache-manifest.536df336a0246be630d7a03af3857849.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56eba748e8b3494ad99c662c68fc7f46",
+    "url": "/koprivica-website/index.html"
+  },
+  {
+    "revision": "c8956120007b601b678a",
+    "url": "/koprivica-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fae168073d9e52efdd95",
+    "url": "/koprivica-website/static/js/2.b18cbed1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/koprivica-website/static/js/2.b18cbed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8956120007b601b678a",
+    "url": "/koprivica-website/static/js/main.e0e142c9.chunk.js"
+  },
+  {
+    "revision": "f4325f4ab9f1569cd064",
+    "url": "/koprivica-website/static/js/runtime-main.d8c639df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/koprivica-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
